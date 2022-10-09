0b2c65d06f5e6a6c19f183d17a4f138b828f840b
Sara DeVries <saradevries@Dougs-MBP-2.MG8702>
2022-10-09 15:15:59 -0500

    probably final one

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/narra")
+def narra():
+    return render_template("narra.html")
+
+@app.route("/nipmuc")
+def nipmuc():
+    return render_template("nipmuc.html")
+
+@app.route("/credit")
+def credit():
+    return render_template("credit.html")
